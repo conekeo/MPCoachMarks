56a9c1a81cf008da3b4c3d24060327e7562c8bbf
Dao Ngoc Anh <anh.dn@citigo.net>
2016-09-16 10:02:20 +0700

    add(assets): add gesture image to project

--- Example/Example/ViewController.m
+++ Example/Example/ViewController.m
@@ -28,11 +28,20 @@
 -(void) showTutorial{
     
     CGRect coachmark1 = CGRectMake(([UIScreen mainScreen].bounds.size.width - 125) / 2, 64, 125, 125);
-    
-    
-    CGRect coachImage1 = CGRectMake(coachmark1.origin.x, coachmark1.origin.y, 30, 30);
+    CGRect coachImage1 = CGRectMake(coachmark1.origin.x, coachmark1.origin.y, 60, 60);
     coachImage1.origin.x += coachmark1.size.width / 2;
     coachImage1.origin.y += coachmark1.size.height / 2;
+    
+    CGRect coachmark2 = CGRectMake(0, 289, [UIScreen mainScreen].bounds.size.width, 44);
+    CGRect coachImage2 = CGRectMake(coachmark2.origin.x, coachmark2.origin.y, 60, 60);
+    coachImage2.origin.x += coachmark2.size.width / 2;
+    coachImage2.origin.y += coachmark2.size.height / 2;
+    
+    
+    CGRect coachmark3 = CGRectMake(0, 333, [UIScreen mainScreen].bounds.size.width, 44);
+    CGRect coachImage3 = CGRectMake(coachmark3.origin.x, coachmark3.origin.y, 60, 60);
+    coachImage3.origin.x += coachmark3.size.width / 2;
+    coachImage3.origin.y += coachmark3.size.height / 2;
     
     // Setup coach marks
     NSArray *coachMarks = @[
@@ -40,37 +49,34 @@
                                 @"rect": [NSValue valueWithCGRect:coachmark1],
                                 @"caption": @"You can put marks over images",
                                 @"shape": [NSNumber numberWithInteger:SHAPE_CIRCLE],
-                                @"position":[NSNumber numberWithInteger:LABEL_POSITION_BOTTOM],
-                                @"alignment":[NSNumber numberWithInteger:LABEL_ALIGNMENT_RIGHT],
-                                @"showArrow":[NSNumber numberWithBool:YES],
-                                kMPCoachMarkImageName: @"swipeleft",
+                                kMPCoachMarkImageName: @"tap",
                                 kMPCoachMarkImageRect: [NSValue valueWithCGRect:coachImage1],
                             },
                             @{
-                                @"rect": [NSValue valueWithCGRect:coachmark1],
-                                @"caption": @"And works with navigations buttons too",
+                                @"rect": [NSValue valueWithCGRect:coachmark2],
+                                @"caption": @"Swipe to left to delete",
                                 @"shape": [NSNumber numberWithInteger:SHAPE_SQUARE],
                                 kMPCoachMarkImageName: @"swiperight",
-                                kMPCoachMarkRect: [NSValue valueWithCGRect:coachImage1]
+                                kMPCoachMarkImageRect: [NSValue valueWithCGRect:coachImage2]
                                 },
                             @{
-                                @"rect": [NSValue valueWithCGRect:coachmark1],
-                                @"caption": @"And works with navigations buttons too",
-                                @"shape": [NSNumber numberWithInteger:SHAPE_CIRCLE],
-                                kMPCoachMarkImageName: @"tap",
-                                kMPCoachMarkImageRect: [NSValue valueWithCGRect:coachImage1]
-                                },
-                            @{
-                                @"rect": [NSValue valueWithCGRect:coachmark1],
-                                @"caption": @"And works with navigations buttons too",
+                                @"rect": [NSValue valueWithCGRect:coachmark3],
+                                @"caption": @"Swip to right to pick",
                                 @"shape": [NSNumber numberWithInteger:SHAPE_SQUARE],
+                                kMPCoachMarkImageName: @"swipeleft",
+                                kMPCoachMarkImageRect: [NSValue valueWithCGRect:coachImage3]
+                                },
+                            @{
+                                @"rect": [NSValue valueWithCGRect:coachmark1],
+                                @"caption": @"And double tap to dismiss",
+                                @"shape": [NSNumber numberWithInteger:SHAPE_CIRCLE],
                                 kMPCoachMarkImageName: @"doubletap",
                                 kMPCoachMarkImageRect: [NSValue valueWithCGRect:coachImage1]
                                 }
                             ];
     
     MPCoachMarks *coachMarksView = [[MPCoachMarks alloc] initWithFrame:self.navigationController.view.bounds coachMarks:coachMarks];
-    coachMarksView.maskCutOutColor = [UIColor colorWithRed:1.000 green:0.000 blue:0.009 alpha:0.504];
+    coachMarksView.maskCutOutColor = [UIColor clearColor];
     
     [self.navigationController.view addSubview:coachMarksView];
     [coachMarksView start];
